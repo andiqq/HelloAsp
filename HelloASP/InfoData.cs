0eb33c17b27b2156bf89903140f882a62f0c1f3b
Dr. Andreas Plagens <andreas@plagens.eu>
2022-09-21 19:51:26 +0200

    first commit

---
+++ HelloASP/InfoData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+
+public class InfoData
+{
+    public int Id { get; set; }
+    public string? name { get; set; }
+}
+
+public class InfoDataContext : DbContext
+{
+    public InfoDataContext(DbContextOptions<InfoDataContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<InfoData>? infoDatas => Set<InfoData>();
+}
+
+
---
+++ HelloASP/Migrations/20220921162043_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HelloASP.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "infoDatas",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_infoDatas", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "infoDatas");
+        }
+    }
+}
